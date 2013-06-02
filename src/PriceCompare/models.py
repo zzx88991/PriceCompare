9735134ed6fe3fddf26e53e4569932fbf7dd91a3
koyabr <koyabr@gmail.com>
2013-06-03 05:00:13 +0800

    Add Good model

--- src/PriceCompare/models.py
+++ src/PriceCompare/models.py
@@ -11,14 +11,18 @@ import chardet
 # ---
 
 class NameField(models.CharField):
-
-	# A username consists of letters, numbers & Hanzi only
 	def to_python(self, value):
 		value = value.strip()
 		if not re.match(ur'^[a-zA-Z0-9\u4e00-\u9fa5]+$', value):
 			raise ValidationError('用户名必须为字母，数字和汉字的组合且不能为空！')
 		return value
 
+class PasswordField(models.CharField):
+	def to_python(self, value):
+		value = value.strip()
+		if not re.match(r'^[_a-zA-Z0-9]{6,}$', value):
+			raise ValidationError('用户名必须为字母，数字和下划线的组合且至少六位！')
+		return value
 
 
 
@@ -28,7 +32,8 @@ class NameField(models.CharField):
 # ---
 
 class User(models.Model):
-	name = NameField(max_length = 20)
+	name = NameField(max_length = 20, unique=True)
+	password = PasswordField(max_length = 15)
 	email = models.EmailField(max_length=254, null=True, blank=True)
 	
 	def __unicode__(self):
@@ -39,4 +44,26 @@ class User(models.Model):
 class UserForm(ModelForm):
 	class Meta:
 		model = User
-		fields = ['name', 'email']
+		fields = ['name', 'password', 'email']
+
+class Good(models.Model):
+
+	# Enum type for sites
+	TAOBAO = 1
+	AMAZON = 2
+	JINGDONG = 3
+	DANGDANG = 4
+	SITE_CHOICES = (
+			(TAOBAO, 'taobao'),
+			(AMAZON, 'amazon'),
+			(JINGDONG, 'jingdong'),
+			(DANGDANG, 'dangdang'),
+		)
+	name = models.CharField(max_length = 100)
+	price = models.DecimalField(max_digits=10, decimal_places=2)
+	site = models.IntegerField(choices=SITE_CHOICES)
+	picUrl = models.URLField(max_length = 200, null=True, blank=True)
+
+	def __unicode__(self):
+		return self.name
+
